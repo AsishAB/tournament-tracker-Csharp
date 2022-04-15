3a8015b2aeae2abdd73131a93db3a1dabb011c50
AsishAB <asish.bishoi@gmail.com>
2022-04-15 23:05:48 +0530

    Added few more changes to the Properties of the Web Form

--- TrackerUI/TournamentViewerFile.Designer.cs
+++ TrackerUI/TournamentViewerFile.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TournamentViewerFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.MaximizeBox = false;
+            this.Name = "TournamentViewerFile";
+            this.Text = "Tournament Viewer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
